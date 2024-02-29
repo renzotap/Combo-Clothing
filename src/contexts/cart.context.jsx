ebf40d4faa3a4e4b74e61db4c88acb50544b5764
renzotap <renzotap@gmail.com>
2024-02-29 13:52:07 -0700

    added reducers and some styles for product-card and category-component 3

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState, useEffect, useReducer } from "react";
-
+import { createContext, useReducer } from "react";
+import { createAction } from "../utils/reducer/reducer.utils";
 export const addCartItem = (cartItems, productToAdd) => {
   //find if cartItems contains productToAdd
   const existingCartItem = cartItems.find(
@@ -98,14 +98,13 @@ export const CartProvider = ({ children }) => {
       0
     );
 
-    dispatch({
-      type: CART_ACTION_TYPES.SET_CART_ITEMS,
-      payload: {
+    dispatch(
+      createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
         cartItems: newCartItems,
         cartTotal: newCartTotal,
         cartCount: newCartCount,
-      },
-    });
+      })
+    );
   };
 
   const addItemToCart = (productToAdd) => {
@@ -122,8 +121,8 @@ export const CartProvider = ({ children }) => {
   };
 
   const setIsCartOpen = (bool) => {
-    dispatch({type: CART_ACTION_TYPES.SET_IS_CART_OPEN, payload: bool})
-  }
+    dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN,bool)
+  )}
 
   const value = {
     isCartOpen,
