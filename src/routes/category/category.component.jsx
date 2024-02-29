551776cdc568a89ec949cbd4ede4b11f3d17c4da
renzotap <renzotap@gmail.com>
2024-02-29 13:36:50 -0700

    added reducers and some styles for product-card and category-component 2

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,6 @@
 
 import { useParams } from 'react-router-dom';
+import './category.styles.scss'
 import { Fragment, useContext, useEffect, useState } from 'react';
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
@@ -16,7 +17,7 @@ const Category = () => {
 
   return (
     <div className='category-preview-container'>
-      <h2 className="title">{category.toUpperCase()}</h2>
+    <h2 className="title">{category.toUpperCase()}</h2>
       <div className="preview">
         {products && products.map((product) => (
           <ProductCard key={product.id} product={product} />
